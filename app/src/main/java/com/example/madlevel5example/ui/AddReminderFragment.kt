199427b85cbc8326a965d1e3991646a4853d8dab
Zakaria Chebti <zakaria@chipta.com>
2020-12-12 11:27:47 +0100

    connected ReminderViewModel to AddReminderFragment

--- app/src/main/java/com/example/madlevel5example/ui/AddReminderFragment.kt
+++ app/src/main/java/com/example/madlevel5example/ui/AddReminderFragment.kt
@@ -6,19 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
-import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel5example.R
+import com.example.madlevel5example.model.Reminder
 import kotlinx.android.synthetic.main.fragment_add_reminder.*
-
-const val REQ_REMINDER_KEY = "req_reminder"
-const val BUNDLE_REMINDER_KEY = "bundle_reminder"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddReminderFragment : Fragment() {
+
+    private val viewModel: ReminderViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +41,7 @@ class AddReminderFragment : Fragment() {
 
         if (reminderText.isNotBlank()) {
             //set the data as fragmentResult, we are listening for REQ_REMINDER_KEY in RemindersFragment!
-            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+            viewModel.insertReminder(Reminder(reminderText))
 
             //"pop" the backstack, this means we destroy
             //this fragment and go back to the RemindersFragment
--- app/src/main/java/com/example/madlevel5example/ui/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel5example/ui/RemindersFragment.kt
@@ -1,26 +1,18 @@
 package com.example.madlevel5example.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel5example.R
 import com.example.madlevel5example.model.Reminder
-import com.example.madlevel5example.repository.ReminderRepository
 import kotlinx.android.synthetic.main.fragment_reminders.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
